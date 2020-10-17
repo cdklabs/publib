63edecf5ff78dc858140ceb081be203c3cb24711
Jon Steinich <jsteinich@gmail.com>
2020-10-17 15:34:06 -0500

    document gpg workaround

--- bin/gpg-workaround.sh
+++ bin/gpg-workaround.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+###
+#
+# This script provides a wrapper around gpg used by maven gpg:sign-and-deploy-file.
+# Many versions of gpg require setting pinentry-mode to loopback to prevent prompting.
+# Generally this can be done in a POM file, but jsii-release-maven tries to keep
+# release details self contained. gpgArguments is also not available as a user property.
+# This script follows:
+# https://stackoverflow.com/questions/60417391/pass-in-list-parameter-to-maven-using-the-cli-gpgsign-and-deploy-file 
+#
+###
+
 gpg --pinentry-mode loopback "$@"
