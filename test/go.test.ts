1c7f986837e58215cb330b14573d98b75f75b098
Eli Polonsky <epolon@amazon.com>
2021-02-08 22:08:14 +0200

    mid work

--- test/go.test.ts
+++ test/go.test.ts
@@ -4,69 +4,33 @@ import * as path from 'path';
 import { GoReleaser } from '../src';
 import * as utils from '../src/utils';
 
-interface ClonerOptions {
-
-  /**
-   * Allows manipulating the source directory.
-   */
-  onSource?: (sourceDir: string) => void;
-
-  /**
-   * Allows manipulating the "cloned" repo.
-   */
-  onTarget?: (targetDir: string) => void;
+function initRepo(repoDir: string) {
+  process.chdir(repoDir);
+  utils.shell('git init');
+  utils.shell('git config user.name jsii-release-tests');
+  utils.shell('git config user.email <>');
+  utils.shell('git add .');
+  utils.shell('git commit -m "Initial commit"');
 }
 
-function mockCloner(options: ClonerOptions = {}) {
-  return (repository: string, targetDir: string) => {
+function createReleaser(fixture: string) {
 
-    // our fixtures represent repositories, so we just copy that over to the target directory
-    // to simulate a clone. since we want to test our sync logic, we need to allow for customization
-    // of both the cloned repo and the generated source. this is enabled by creating a copy of the fixture
-    // and using that as the source, along with the 'onSource' and 'onTarget' hooks.
-    const fixture = path.join(__dirname, '__fixtures__', repository.split('/')[1]);
-    const sourceDir = path.join(fs.mkdtempSync(os.tmpdir()), path.basename(fixture));
+  const fixturePath = path.join(__dirname, '__fixtures__', fixture);
+  const sourceDir = path.join(fs.mkdtempSync(os.tmpdir()), path.basename(fixture));
+  utils.shell(`cp -r ${fixturePath} ${sourceDir}`);
 
-    utils.shell(`cp -r ${fixture} ${sourceDir}`);
-    if (options.onSource) {
-      options.onSource(sourceDir);
-    }
-    utils.shell(`cp -r ${sourceDir} ${targetDir}`);
-    if (options.onTarget) {
-      options.onTarget(targetDir);
-    }
-
-    process.chdir(targetDir);
-
-    utils.shell('git init');
-    utils.shell('git config user.name jsii-release-tests');
-    utils.shell('git config user.email <>');
-    utils.shell('git add .');
-    utils.shell('git commit -m "Initial commit"');
+  // create the releaser with a copy of the fixture to allow
+  // source customization.
+  const releaser = new GoReleaser(sourceDir);
+  (releaser as any)._cloner = function(_: string, targetDir: string) {
+    // the cloned repo is always the original fixture.
+    utils.shell(`cp -r ${fixturePath} ${targetDir}`);
+    initRepo(targetDir);
   };
+
+  return { releaser, sourceDir };
 }
 
-// a cloner that adds a file to the cloned repo. since this file is missing
-// from the source, it simulates a deleted file.
-const deletedFileCloner = (submodule?: string) => {
-  return mockCloner({
-    onTarget: (targetDir: string) => {
-    // add a file so we have changes
-      fs.writeFileSync(path.join(targetDir, submodule ?? '', 'file'), 'test');
-    },
-  });
-};
-
-// a cloner that adds a file to the source. since this file is missing
-// from the repo, it simulates an added file.
-const addedFileCloner = (submodule?: string) => {
-  return mockCloner({
-    onSource: (sourceDir: string) => {
-    // add a file so we have changes
-      fs.writeFileSync(path.join(sourceDir, submodule ?? '', 'file'), 'test');
-    },
-  });
-};
 
 beforeEach(() => {
   process.env.DRY_RUN = 'True';
@@ -74,8 +38,9 @@ beforeEach(() => {
 
 test('top-level produces a tag without prefix', () => {
 
-  const releaser = new GoReleaser(`${__dirname}/__fixtures__/top-level`);
-  (releaser as any)._cloner = addedFileCloner();
+  const { releaser, sourceDir } = createReleaser('top-level');
+
+  fs.writeFileSync(path.join(sourceDir, 'file'), 'test');
   const release = releaser.release();
 
   expect(release.tags).toEqual(['v1.1.0']);
@@ -84,8 +49,9 @@ test('top-level produces a tag without prefix', () => {
 
 test('sub-modules produce tags with prefixes', () => {
 
-  const releaser = new GoReleaser(`${__dirname}/__fixtures__/sub-modules`);
-  (releaser as any)._cloner = addedFileCloner('module1');
+  const { releaser, sourceDir } = createReleaser('sub-modules');
+
+  fs.writeFileSync(path.join(sourceDir, 'module1', 'file'), 'test');
   const release = releaser.release();
 
   expect(release.tags).toEqual(['module1/v1.1.0', 'module2/v1.1.0']);
@@ -94,8 +60,9 @@ test('sub-modules produce tags with prefixes', () => {
 
 test('combined procudes tags both with an without a prefix', () => {
 
-  const releaser = new GoReleaser(`${__dirname}/__fixtures__/combined`);
-  (releaser as any)._cloner = addedFileCloner();
+  const { releaser, sourceDir } = createReleaser('combined');
+
+  fs.writeFileSync(path.join(sourceDir, 'file'), 'test');
   const release = releaser.release();
 
   expect(release.tags).toEqual(['v1.1.0', 'module1/v1.1.0', 'module2/v1.1.0']);
@@ -104,8 +71,9 @@ test('combined procudes tags both with an without a prefix', () => {
 
 test('multi-version produces tags with different versions', () => {
 
-  const releaser = new GoReleaser(`${__dirname}/__fixtures__/multi-version`);
-  (releaser as any)._cloner = addedFileCloner('module2');
+  const { releaser, sourceDir } = createReleaser('multi-version');
+  fs.writeFileSync(path.join(sourceDir, 'module1', 'file'), 'test');
+
   const release = releaser.release();
 
   expect(release.tags).toEqual(['module1/v1.1.0', 'module2/v1.2.0']);
@@ -122,8 +90,9 @@ test('throws is domain if module repo domain is not github.com', () => {});
 
 test('considers deleted files', () => {
 
-  const releaser = new GoReleaser(`${__dirname}/__fixtures__/top-level`);
-  (releaser as any)._cloner = deletedFileCloner();
+  const { releaser, sourceDir } = createReleaser('top-level');
+
+  fs.unlinkSync(path.join(sourceDir, 'source.go'));
   const release = releaser.release();
 
   expect(release.tags).toEqual(['v1.1.0']);
@@ -134,8 +103,9 @@ test('considers deleted modules', () => {});
 
 test('considers added files', () => {
 
-  const releaser = new GoReleaser(`${__dirname}/__fixtures__/top-level`);
-  (releaser as any)._cloner = addedFileCloner();
+  const { releaser, sourceDir } = createReleaser('top-level');
+
+  fs.writeFileSync(path.join(sourceDir, 'file'), 'test');
   const release = releaser.release();
 
   expect(release.tags).toEqual(['v1.1.0']);
