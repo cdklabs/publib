72e37d53fae9e17bc6d72e098877d7e9438ed616
Elad Ben-Israel <benisrae@amazon.com>
2022-02-10 17:25:56 +0200

    feat: rename to publib
    This project is now called `publib` and released under this name to npm.
    
    We continue to release this as `jsii-release` for backwards compatibility and all the `jsii-release-xxx` commands still work (albeit issue a deprecation warning).

--- .projenrc.js
+++ .projenrc.js
@@ -1,17 +1,16 @@
+const { readdirSync } = require('fs');
 const { typescript } = require('projen');
 
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'master',
-  name: 'jsii-release',
+  name: 'publib',
   description: 'Release jsii modules to multiple package managers',
   releaseToNpm: true,
-  keywords: ['jsii'],
-  repository: 'https://github.com/aws/jsii-release.git',
+  repository: 'https://github.com/aws/publib.git',
   authorName: 'Amazon Web Services',
   authorOrganization: true,
   authorUrl: 'https://aws.amazon.com',
-  homepage: 'https://github.com/aws/jsii-release',
-  projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
+  homepage: 'https://github.com/aws/publib',
   autoApproveOptions: {
     allowedUsernames: ['cdklabs-automation'],
     secret: 'GITHUB_TOKEN',
@@ -19,25 +18,25 @@ const project = new typescript.TypeScriptProject({
   autoApproveUpgrades: true,
 });
 
-// create tarball and move to dist/js so release workflow can pick it up from there.
-project.projectBuild.postCompileTask.exec('yarn pack');
-project.projectBuild.postCompileTask.exec('mkdir -p dist/js');
-project.projectBuild.postCompileTask.exec('mv ./jsii-release-v*.tgz dist/js');
-
 // we can't use 9.x because it doesn't work with node 10.
 const fsExtraVersion = '^8.0.0';
 
 project.addDeps('shlex', `fs-extra@${fsExtraVersion}`, `@types/fs-extra@${fsExtraVersion}`);
 
-const publib = project.addTask('package-as-publib');
-publib.exec('cp package.json package.json.bak');
-publib.exec('node ./scripts/update-package-name.js publib');
-publib.exec('yarn pack');
-publib.exec('mv ./publib*.tgz dist/js');
-publib.exec('cp package.json.bak package.json');
-publib.exec('rm package.json.bak');
+const legacy = project.addTask('package-legacy');
+legacy.exec('cp package.json package.json.bak');
+legacy.exec('node ./scripts/update-package-name.js jsii-release');
+legacy.exec('npm pack');
+legacy.exec('mv ./jsii-release*.tgz dist/js');
+legacy.exec('cp package.json.bak package.json');
+legacy.exec('rm package.json.bak');
 
-project.packageTask.spawn(publib);
+project.packageTask.spawn(legacy);
 
+// map all "jsii-release-*" to "jsii-release-shim" as executables
+for (const f of readdirSync('./bin').filter(file => file.startsWith('publib'))) {
+  const shim = ['jsii-release', f.split('-')[1]].filter(x=>x).join('-');
+  project.addBins({ [shim]: './bin/jsii-release-shim' });
+}
 
 project.synth();
